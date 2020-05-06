5fa3ad861140500bcbdebd2a02192c23f08a0b33
Karmit Personal <karmit199@gmail.com>
2020-05-06 19:59:41 -0400

    Initial empty files added.

--- new-frontend/src/app/app.module.ts
+++ new-frontend/src/app/app.module.ts
@@ -2,10 +2,24 @@ import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 
 import {AppComponent} from "./app.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {AnnualLeaveComponent} from "./pages/annual-leave/annual-leave.component";
+import {SickLeaveComponent} from "./pages/sick-leave/sick-leave.component";
+import {AdminPageComponent} from "./pages/admin-page/admin-page.component";
+import {DashboardComponent} from "./pages/dashboard/dashboard.component";
+import {ToolbarComponent} from "./pages/toolbar/toolbar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AnnualLeaveComponent,
+    SickLeaveComponent,
+    AdminPageComponent,
+    DashboardComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
---
+++ new-frontend/src/app/pages/annual-leave/annual-leave.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: "app-annual-leave",
+  templateUrl: "./annual-leave.component.html",
+  styleUrls: ["./annual-leave.component.css"]
+})
+export class AnnualLeaveComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
